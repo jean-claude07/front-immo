60ec4e5e6a3122f516f998a466628addf620d7e6
jean-claude07 <jeclaude78@gmail.com>
2021-03-11 13:05:59 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/homepages/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>bonjour dago immo</h1>
+    <div >
+      <Homepage/>
     </div>
   );
 }
